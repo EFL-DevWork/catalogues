c9e95ef4e5a6aa88fbdaf0e9f2d17ed0b6d62a94
sruthiar14 <sruthi.ar@thoughtworks.com>
2023-03-09 14:14:17 +0530

    [Cart7] refactor:use lambok[Radhika|Sruthi]

--- src/main/java/com/cart/cartCatalogues/model/Category.java
+++ src/main/java/com/cart/cartCatalogues/model/Category.java
@@ -2,66 +2,17 @@ package com.cart.cartCatalogues.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @Entity
+@AllArgsConstructor
 public class Category {
-
+    @Getter
     @Id
     Long id;
     String name;
     String slug;
     String description;
-
-    public Category() {
-    }
-
-    public Category(Long id, String name, String slug, String description) {
-        this.id = id;
-        this.name = name;
-        this.slug = slug;
-        this.description = description;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSlug() {
-        return slug;
-    }
-
-    public void setSlug(String slug) {
-        this.slug = slug;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    @Override
-    public String toString() {
-        return "Category{" +
-                "category_id=" + id +
-                ", name='" + name + '\'' +
-                ", slug='" + slug + '\'' +
-                ", description='" + description + '\'' +
-                '}';
-    }
 
 }
--- src/main/java/com/cart/cartCatalogues/model/Product.java
+++ src/main/java/com/cart/cartCatalogues/model/Product.java
@@ -4,10 +4,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @Entity
+@AllArgsConstructor
 public class Product {
-
+    @Getter
     @Id
     long product_id;
     String product_info;
@@ -17,77 +20,4 @@ public class Product {
     @ManyToOne(fetch = FetchType.LAZY)
     Category category;
 
-    public String getProduct_info() {
-        return product_info;
-    }
-
-    public void setProduct_info(String product_info) {
-        this.product_info = product_info;
-    }
-
-    public Category getCategory() {
-        return category;
-    }
-
-    public void setCategory(Category category) {
-        this.category = category;
-    }
-
-    public Product() {
-    }
-
-    public Product(long product_id, String product_info, String name, String image_url, Long price, Category category) {
-        this.product_id = product_id;
-        this.product_info = product_info;
-        this.name = name;
-        this.image_url = image_url;
-        this.price = price;
-        this.category = category;
-    }
-
-    public long getProduct_id() {
-        return product_id;
-    }
-
-    public void setProduct_id(int product_id) {
-        this.product_id = product_id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getImage_url() {
-        return image_url;
-    }
-
-    public void setImage_url(String image_url) {
-        this.image_url = image_url;
-    }
-
-
-
-    public Long getPrice() {
-        return price;
-    }
-
-    public void setPrice(Long price) {
-        this.price = price;
-    }
-
-    @Override
-    public String toString() {
-        return "Products{" +
-                "product_id=" + product_id +
-                ", product_info='" + product_info + '\'' +
-                ", name='" + name + '\'' +
-                ", image_url='" + image_url + '\'' +
-                ", price=" + price +
-                ", category=" + category +
-                '}';
-    }
 }
