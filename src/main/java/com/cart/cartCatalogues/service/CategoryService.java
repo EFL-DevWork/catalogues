392b9e1d42c6d914836316e4be605db7ab3e399f
radhikasinghal <radhikasinghal2504@gmail.com>
2023-03-13 17:27:17 +0530

    [Cart7] Implement ListMixin [Sruthi|Radhika]

--- src/main/java/com/cart/cartCatalogues/service/CategoryService.java
+++ src/main/java/com/cart/cartCatalogues/service/CategoryService.java
@@ -1,20 +1,28 @@
 package com.cart.cartCatalogues.service;
 
+import com.cart.cartCatalogues.commonPackage.ListMixin;
 import com.cart.cartCatalogues.model.Category;
 import com.cart.cartCatalogues.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.IntFunction;
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
+import java.util.stream.Stream;
 
 @Service
-public class CategoryService {
+public class CategoryService implements ListMixin<Category> {
+
+    private List<Category> categories=new ArrayList<>();
     @Autowired
     CategoryRepository categoryRepository;
 
     public Category insertCategories(Category category) {
         categoryRepository.save(category);
+        categories.add(category);
         return category;
     }
 
@@ -31,4 +39,164 @@ public class CategoryService {
         categoryRepository.deleteById(id);
     }
 
+
+    @Override
+    public List<Category> getRecords() {
+        return categories;
+    }
+
+    @Override
+    public int size() {
+        return ListMixin.super.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return ListMixin.super.isEmpty();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator<Category> iterator() {
+        return null;
+    }
+
+    @Override
+    public void forEach(Consumer<? super Category> action) {
+        ListMixin.super.forEach(action);
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return null;
+    }
+
+    @Override
+    public <T> T[] toArray(IntFunction<T[]> generator) {
+        return ListMixin.super.toArray(generator);
+    }
+
+    @Override
+    public boolean add(Category category) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends Category> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends Category> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeIf(Predicate<? super Category> filter) {
+        return ListMixin.super.removeIf(filter);
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void replaceAll(UnaryOperator<Category> operator) {
+        ListMixin.super.replaceAll(operator);
+    }
+
+    @Override
+    public void sort(Comparator<? super Category> c) {
+        ListMixin.super.sort(c);
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Category get(int index) {
+        return null;
+    }
+
+    @Override
+    public Category set(int index, Category element) {
+        return null;
+    }
+
+    @Override
+    public void add(int index, Category element) {
+
+    }
+
+    @Override
+    public Category remove(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator<Category> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<Category> listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List<Category> subList(int fromIndex, int toIndex) {
+        return null;
+    }
+
+    @Override
+    public Spliterator<Category> spliterator() {
+        return ListMixin.super.spliterator();
+    }
+
+    @Override
+    public Stream<Category> stream() {
+        return ListMixin.super.stream();
+    }
+
+    @Override
+    public Stream<Category> parallelStream() {
+        return ListMixin.super.parallelStream();
+    }
 }
--- src/main/java/com/cart/cartCatalogues/service/ProductService.java
+++ src/main/java/com/cart/cartCatalogues/service/ProductService.java
@@ -1,22 +1,29 @@
 package com.cart.cartCatalogues.service;
 
+import com.cart.cartCatalogues.commonPackage.ListMixin;
 import com.cart.cartCatalogues.model.Product;
 import com.cart.cartCatalogues.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import java.util.*;
 
-import java.util.List;
 import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.IntFunction;
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
+import java.util.stream.Stream;
 
 @Service
-public class ProductService {
+public class ProductService implements ListMixin<Product> {
+    List<Product> products=new ArrayList<>();
     @Autowired
     ProductRepository productRepository;
 
     public Product insertProducts(Product product) {
         productRepository.save(product);
+        products.add(product);
         return product;
     }
 
@@ -33,4 +40,163 @@ public class ProductService {
     }
 
 
+    @Override
+    public List<Product> getRecords() {
+        return products;
+    }
+
+    @Override
+    public int size() {
+        return products.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return products.isEmpty();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator<Product> iterator() {
+        return null;
+    }
+
+    @Override
+    public void forEach(Consumer<? super Product> action) {
+        ListMixin.super.forEach(action);
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return null;
+    }
+
+    @Override
+    public <T> T[] toArray(IntFunction<T[]> generator) {
+        return ListMixin.super.toArray(generator);
+    }
+
+    @Override
+    public boolean add(Product product) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends Product> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends Product> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeIf(Predicate<? super Product> filter) {
+        return ListMixin.super.removeIf(filter);
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void replaceAll(UnaryOperator<Product> operator) {
+        ListMixin.super.replaceAll(operator);
+    }
+
+    @Override
+    public void sort(Comparator<? super Product> c) {
+        ListMixin.super.sort(c);
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Product get(int index) {
+        return null;
+    }
+
+    @Override
+    public Product set(int index, Product element) {
+        return null;
+    }
+
+    @Override
+    public void add(int index, Product element) {
+
+    }
+
+    @Override
+    public Product remove(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator<Product> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<Product> listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List<Product> subList(int fromIndex, int toIndex) {
+        return null;
+    }
+
+    @Override
+    public Spliterator<Product> spliterator() {
+        return ListMixin.super.spliterator();
+    }
+
+    @Override
+    public Stream<Product> stream() {
+        return ListMixin.super.stream();
+    }
+
+    @Override
+    public Stream<Product> parallelStream() {
+        return ListMixin.super.parallelStream();
+    }
 }
